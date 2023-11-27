db23f8979c557ddd6a2ec998711d754fc00c4958
Diyan Simeonov <didolaneca@yahoo.com>
2023-11-27 14:32:09 -0600

    Added "Lorem Ipsum" for visualization of the container/wrapper

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+	return (
+		<div>
+			<h1>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+				obcaecati, totam facere numquam, quod, a dolorem excepturi vel
+				quos deserunt fuga aperiam. Facere quasi, quibusdam expedita
+				omnis est amet! Repellat!
+			</h1>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
+			porro sunt excepturi perferendis assumenda. Doloribus ipsam
+			molestiae ex aliquid, pariatur eum consequatur aliquam alias magni,
+			nemo, exercitationem eaque mollitia commodi.
+			<br />
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
+			porro sunt excepturi perferendis assumenda. Doloribus ipsam
+			molestiae ex aliquid, pariatur eum consequatur aliquam alias magni,
+			nemo, exercitationem eaque mollitia commodi.
+			<br />
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
+			porro sunt excepturi perferendis assumenda. Doloribus ipsam
+			molestiae ex aliquid, pariatur eum consequatur aliquam alias magni,
+			nemo, exercitationem eaque mollitia commodi.
+		</div>
+	);
 }
