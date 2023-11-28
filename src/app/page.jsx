3b9a557caf2643f3412017d0ae504efa0a92bc1f
Diyan Simeonov <didolaneca@yahoo.com>
2023-11-28 16:06:37 -0600

    Added the Featured, CategoryList, CardList,  Menu components to the page and simple CSS styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,18 @@
+import Featured from "@/components/featured/Featured";
 import styles from "./homepage.module.css";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
 
 export default function Home() {
 	return (
-		<div>
-			<h1>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
-				obcaecati, totam facere numquam, quod, a dolorem excepturi vel
-				quos deserunt fuga aperiam. Facere quasi, quibusdam expedita
-				omnis est amet! Repellat!
-			</h1>
+		<div className={styles.container}>
+			<Featured />
+			<CategoryList />
+			<div className={styles.content}>
+				<CardList />
+				<Menu />
+			</div>
 		</div>
 	);
 }
