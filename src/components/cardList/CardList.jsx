4e7ab56c87254cfce4d00995c00b470e03ce688d
Diyan Simeonov <didolaneca@yahoo.com>
2023-11-28 16:07:00 -0600

    Added the Pagination component to the CardList

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import styles from "./footer.module.css";
+import styles from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
 
 const CardList = () => {
-	return <div className="styles.container">CardList</div>;
+	return (
+		<div className="styles.container">
+			CardList
+			<Pagination />
+		</div>
+	);
 };
 
 export default CardList;
