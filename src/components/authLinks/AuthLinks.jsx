70245004dddd9ec2e8cc1b584f72e60b1398de1f
Diyan Simeonov <didolaneca@yahoo.com>
2023-11-29 14:51:39 -0600

    Fixed the "styles" import statement

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { styles } from "./AuthLinks.module.css";
+import styles from "./authLinks.module.css";
 
 const AuthLinks = () => {
 	return <div className={styles.container}>AuthLinks</div>;
--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { styles } from "./themeToggle.module.css";
+import styles from "./themeToggle.module.css";
 
 const ThemeToggle = () => {
 	return <div className={styles.container}>ThemeToggle</div>;
